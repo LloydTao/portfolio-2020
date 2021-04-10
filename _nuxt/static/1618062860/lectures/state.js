7645ebe7dc18e7df50bbf342a5002427877a86ea
LloydTao <41923457+LloydTao@users.noreply.github.com>
2021-04-10 13:54:26 +0000

    Deploying to build from  @ 76da5e94c26daaa24090fe85410cd5255d461fbd 🚀

---
+++ _nuxt/static/1618062860/lectures/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618062860",layout:"default",error:a,serverRendered:true,routePath:"\u002Flectures",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d3d01f5e"}}}}(null));
