09b51f88350fd653363f14dc273338963c11cafe
LloydTao <41923457+LloydTao@users.noreply.github.com>
2021-04-10 01:55:34 +0000

    Deploying to build from  @ 0dc987fb4b00ec07a5f8a388e57247fb3e05f7ac 🚀

---
+++ _nuxt/static/1618019730/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Flectures","\u002Fopen-source","\u002Fportfolio"]})
---
+++ _nuxt/static/1618019730/open-source/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618019730",layout:"default",error:a,serverRendered:true,routePath:"\u002Fopen-source",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3e219cf1"}}}}(null));
