f1602491db963c370ba9d311181f6c72441d8ae4
LloydTao <41923457+LloydTao@users.noreply.github.com>
2021-05-03 15:49:41 +0000

    Deploying to build from  @ db4421e9bb1403196c96c73213bb87af6957cc86 🚀

--- _nuxt/static/1619140960/portfolio/test-project/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1619140960",layout:"default",error:a,serverRendered:true,routePath:"\u002Fportfolio\u002Ftest-project",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"729b2ca0"}}}}(null));
